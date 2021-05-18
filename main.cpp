415d9edd850d64ed988fe3d215d7f6fbb796fae3
Alphexus <52839154+Alphexus@users.noreply.github.com>
2021-05-17 20:32:40 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 /*
-    A demonstration of the A* Search Algorithm. 
+    A demonstration of the A* Search Algorithm.
+    All work is written by the author except C++ STD library functions.
 */
 
 #include <iostream>
@@ -10,10 +11,17 @@
 #include <limits>
 #include <queue>
 #include <iterator>
+#include <stdlib.h>
+#include <ctime>
+#include <unistd.h>
+#include <termios.h>
+#include <algorithm>
 
-const int COLLUMNS = 80, ROWS = 30;
-const int Y_START = 5, X_START = 7;
-const int Y_GOAL = 20, X_GOAL = 50;
+const int RECONSTRUCTION_DELAY_MS = 400; // change this to 0 to see how fast it constructs the path.  
+const int COLLUMNS = 80; const int ROWS = 30;
+int Y_START = 15; int X_START = 40;
+int Y_AGENT = Y_START; int X_AGENT = X_START-1;
+int Y_GOAL; int X_GOAL;
 
 int grid[ROWS][COLLUMNS];
 
@@ -26,7 +34,7 @@ struct nodeComparison {
         const noexcept
     { 
         return a.second > b.second; 
-    } 
+    }
 }; 
 
 void initiate();
@@ -36,25 +44,85 @@ std::vector<nodeTemplate> search();
 std::vector<nodeTemplate> tracePath(std::map<nodeTemplate, nodeTemplate> cameFrom, nodeTemplate current);
 std::vector<nodeTemplate> getNeighbors(nodeTemplate node);
 
-int main() {
-    initiate();
-    grid[Y_START][X_START] = 1;
-    grid[Y_GOAL][X_GOAL] = 2;
+int steps = 0;
 
+int main() {
+    struct termios old_tio, new_tio;
+
+    // Detect keypresses without having to press enter 
+    tcgetattr(STDIN_FILENO,&old_tio);
+    new_tio=old_tio;
+    new_tio.c_lflag &=(~ICANON & ~ECHO);
+    tcsetattr(STDIN_FILENO,TCSANOW,&new_tio);
+
+    initiate();
+    render();
+
+    char c;
+    while (c != 'p') {
+        c = getchar();
+        short int lastState = grid[Y_AGENT][X_AGENT];
+        switch (c) {
+            case 'w':
+                Y_AGENT = std::clamp(Y_AGENT-1, 0, ROWS);
+                break;
+            case 'a':
+                X_AGENT = std::clamp(X_AGENT-1, 0, COLLUMNS);
+                break;
+            case 's':
+                Y_AGENT = std::clamp(Y_AGENT+1, 0, ROWS);
+                break;
+            case 'd':
+                X_AGENT = std::clamp(X_AGENT+1, 0, COLLUMNS);
+                break;
+        }
+        short int newState = grid[Y_AGENT][X_AGENT];
+        if (newState != 2 || newState != 7) {
+            grid[Y_AGENT][X_AGENT] = 3;
+            render();
+        } else {
+          grid[Y_AGENT][X_AGENT] = lastState;
+        }
+    }
+    
     std::vector<nodeTemplate> path = search();
+    nodeTemplate lastNode(Y_START, X_START);
+    short int dir = 1;
     for (std::vector<nodeTemplate>::iterator node = path.begin(); node != path.end(); ++node) {
-        grid[node->first][node->second] = 1;
+        // get direction delta for displaying array in correct orientation
+        if (node->first > lastNode.first) {
+            dir = 6; // up movement
+        } else if (node->first < lastNode.first) {
+            dir = 5; // down movement
+        } else if (node->second > lastNode.second) {
+            dir = 4; // right movement
+        } else if (node->second < lastNode.second) {
+            dir = 1; // left movement
+        }
+        lastNode = *node;
+        if (grid[node->first][node->second] != 7) {
+            grid[node->first][node->second] = dir;
+        }
         render();
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        steps++;
+        std::this_thread::sleep_for(std::chrono::milliseconds(RECONSTRUCTION_DELAY_MS)); // wait to see each step
     }
 }
 
 void initiate() {
+    std::srand(std::time(nullptr));
+    Y_GOAL = std::rand() % ROWS;
+    X_GOAL = std::rand() % COLLUMNS;
+    steps = 0;
+
     for (int y = 0; y < ROWS; y++) {
         for (int x = 0; x < COLLUMNS; x++) {
             grid[y][x] = 0;
         }
     }
+
+    grid[Y_START][X_START] = 7;
+    grid[Y_GOAL][X_GOAL] = 2;
 }
 
 int heuristic(nodeTemplate goal, nodeTemplate node) {
@@ -97,17 +165,36 @@ void render() {
     for (int y = 0; y < ROWS; y++) {
         for (int x = 0; x < COLLUMNS; x++) {
             int state = grid[y][x];
-            if (state == 0) { // Land
-                std::cout << "*";
-            } else if (state == 1) { // Agent
-                std::cout << "#";
-            } else if (state == 2) { // Goal
-                std::cout << "★";
+            switch (state) {
+                case 0:
+                    std::cout << "*"; // ground
+                    break;
+                case 1:
+                    std::cout << "⮜"; // left arrow
+                    break;
+                case 2:
+                    std::cout << "★"; // goal
+                    break;
+                case 3:
+                    std::cout << "⬜"; // wall
+                    break;
+                case 4:
+                    std::cout << "⮞"; // right arrow
+                    break;
+                case 5:
+                    std::cout << "⮝"; // up arrow
+                    break;
+                case 6:
+                    std::cout << "⮟"; // down arrow
+                    break;
+                case 7:
+                    std::cout << "◉";
             }
         }
         std::cout << "\n";
     }
-}
+    std::cout << "Steps: " << steps << "\n";
+} 
 
 std::vector<nodeTemplate> search() {
     nodeTemplate start = std::make_pair(Y_START, X_START);
@@ -131,12 +218,8 @@ std::vector<nodeTemplate> search() {
     };
 
     while (!frontier.empty()) {
-       // std::this_thread::sleep_for(std::chrono::milliseconds(50));
         nodeTemplate n = frontier.top().first;
         frontier.pop(); 
-        //grid[n.first][n.second] = 1;
-        //render();
-        std::cout << n.first << " | " << n.second << "\n";
         if (n.first == goal.first && n.second == goal.second) { // found goal
             std::cout << "Found goal\n";
             return tracePath(cameFrom, n);
